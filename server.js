b4a25e0ca854c282e22ee1732f976461528c66f6
Jamesbillard12 <jamesbillard12@hotmail.com>
2017-07-31 15:41:24 -0700

    off to testing?

--- server.js
+++ server.js
@@ -12,16 +12,11 @@ const app = express();
 
 app.use(morgan('dev'));
 
-app.get('/test', function(req, res) {
-  debug('GET: /test');
-  res.json({msg: 'you have tested a GET req'});
-});
-
 app.post('/api/beer', jsonParser, function (req, res, next) {
-  debug('POST: /apo/beer');
+  debug('POST: /api/beer');
 
   Beer.createBeer(req.body)
-  .then( beer => res.json(beer))
+  .then( beer => res.status(200).json(beer))
   .catch( err => next(err));
 });
 
@@ -35,8 +30,33 @@ app.get('/api/beer', function(req, res, next) {
 app.delete('/api/beer', function(req, res, next) {
   debug('DELETE: /api/beet');
   Beer.deleteBeer(req.query.id)
-  .then( () => res.send('Deleted beer'))
+  .then( () => res.status(204).send('Beer Deleted'))
   .catch( err => next(err));
+});
+
+app.use(function(err, req, res, next){
+  debug('error middleware');
+  console.error(err.message);
+
+  if(err.status){
+    res.status(err.status).send(err.name);
+    return;
+  }
+
+  if(res.status(404)){
+    res.status(404).send('Not Found');
+    return;
+  }
+
+  if(err.status(400)){
+    res.status(400).send('Bad Request');
+    return;
+  }
+
+
+
+  err = createError(500, err.message);
+  res.status(err.status).send(err.name);
 });
 
 
