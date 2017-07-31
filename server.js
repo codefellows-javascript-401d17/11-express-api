5959e04b906cd28fac5555cf22d9a6297556c798
Luis Manzo <manzosk3@gmail.com>
2017-07-31 14:27:54 -0700

    uses JSONparser to parse POST and PUT routes

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 'use strict';
 const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 3000;
 const Drink = require('./model/drink.js');
+const jsonParser = require('body-parser').json();
+const PORT = process.env.PORT || 3000;
+const app = express();
 
-app.listen(function(req, rsp) {
+app.get('/test', function(req, rsp) {
+  
+})
+
+app.post('/drinks', jsonParser, function(req, rsp, next) {
+  rsp.json(req.body);
+});
+
+app.put('/drinks/:id', jsonParser, function(req, rsp, next) {
+  rsp.json(req.body);
+});
+
+app.listen(PORT, function(req, rsp) {
   console.log('listening on port', PORT);
 })
