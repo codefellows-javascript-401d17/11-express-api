984fdbb12f98f5233cebaedece186c73c806f2c9
ohjonah <ohjonah@gmail.com>
2017-07-31 15:36:00 -0700

    Modularizes and adds tests

---
+++ model/bake.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const uuidv4 = require('uuid/v4');
+const createError = require('http-errors');
+const debug = require('debug')('bake:bake');
+const storage = require('../lib/storage.js');
+
+const Bake = module.exports = function(bakedGood, description, calories) {
+  if (!bakedGood) throw new createError(400, 'expected baked good');
+  if (!description) throw new createError(400, 'expected description');
+  if (!calories) throw new createError(400, 'expected expected calories');
+
+  this.id = uuidv4();
+  this.bakedGood = bakedGood;
+  this.description = description;
+  this.calories = calories;
+};
+
+Bake.createBakedGood = function(_bake) {
+  debug('createBakedGood');
+
+  try {
+    let bake = new Bake(_bake.bakedGood, _bake.description, _bake.calories);
+    return storage.createItem('bake', bake);
+  } catch (err) {
+    return Promise.reject(err);
+  }
+};
+
+Bake.fetchBakedGood = function(id) {
+  debug('fetchBakedGood');
+
+  return storage.fetchItem('bake', id);
+};
+
