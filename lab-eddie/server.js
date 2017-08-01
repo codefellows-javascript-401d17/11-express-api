1e47e241ec5af6c0826d93923f99501f034de6a0
Edwin Del Rio <edwinjdelrio@gmail.com>
2017-07-31 19:36:13 -0700

    Configured server.js to use express

--- lab-eddie/server.js
+++ lab-eddie/server.js
@@ -1,18 +1,18 @@
-'use strict'
+'use strict';
 
-const http = require('http');
-const Router = require('./lib/router.js');
-const PORT = process.env.PORT || 5000;
-const modelPaths = require('./lib/model-paths.js')
-const router = new Router();
+const express = require('express');
+const morgan = require('morgan');
+const createError = require('http-errors');
+const jasonParser = require('body-parser').json();
+const debug = require('debug')('note:server')
+const PORT = process.env.PORT || 3000;
 
-const modelKeys = Object.keys(modelPaths.models);
-modelKeys.forEach(key => {
-  modelPaths.allRoutes(key, router);
-})
+const app = express();
 
-const server = http.createServer(router.route());
+app.use(morgan('dev'));
+
+
   
-server.listen(PORT, () => {
-  console.log('server on at port:', PORT);
+app.listen(PORT, () => {
+  debug(console.log('server on at port:', PORT));
 });
