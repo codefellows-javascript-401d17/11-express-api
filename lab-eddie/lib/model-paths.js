97de351bd0fb3f7385dc6e661b3bfcf65f13fc41
Edwin Del Rio <edwinjdelrio@gmail.com>
2017-07-31 20:45:14 -0700

    refactored model-paths to use debug and express's native methods

--- lab-eddie/lib/model-paths.js
+++ lab-eddie/lib/model-paths.js
@@ -1,12 +1,14 @@
 'use strict';
 
+const debug = require('debug')('app:model-paths');
+const jsonParser = require('body-parser').json();
 const storage = require('./storage.js');
-const header = require('./header.js')
 const Person = require('../model/person.js');
 const Car = require('../model/car.js');
-const Dog = require('../model/dog.js')
-const Employee = require('../model/employee.js')
-const Character = require('../model/character.js')
+const Dog = require('../model/dog.js');
+const Employee = require('../model/employee.js');
+const Character = require('../model/character.js');
+
 
 
 const modelRoutes = module.exports = {};
@@ -17,85 +19,66 @@ modelRoutes.models = {
   dog: Dog,
   employee: Employee,
   character: Character
-}
+};
 
 
 modelRoutes.allRoutes = function(model, router) {
   modelRoutes.modelGet(model, router);
   modelRoutes.modelDelete(model, router);
   modelRoutes.modelPost(model, router);
-}
+};
 
 
 modelRoutes.modelGet = function(model,router) {
-  router.get(`/api/${model}`, function(req, res) {
+  router.get(`/api/${model}`,jsonParser, function(req, res, next) {
+    debug(`GET: api/${model}`);
     if (req.url.query.id) {
       storage.fetchItem(`${model}`, req.url.query.id)
-      .then( item => {
-        header.appHeader(res, 200, item);
-      })
-      .catch( err => {
-        console.error(err);
-        header.textHeader(res, 404, `${model} not found!`)
-      });
+      .then(item => res.JSON(item))
+      .catch(err => next(err));
 
       return;
+
     } else if (req.url.query) {
       storage.fetchItem(`${model}`)
-      .then( item => {
-        header.appHeader(res, 200, item);
-      })
-      .catch( err => {
-        console.error(err);
-        header.textHeader(res, 400, 'Bad request!')
-      });
+      .then(item => res.JSON(item))
+      .catch(err => next(err));
 
       return;
     };
-
-    header.textHeader(res, 400, 'Bad request!')
   });
 };
 
 modelRoutes.modelPost = function(model, router) {
-  router.post(`/api/${model}`, function(req, res) {
-    try {
-      let params = [];
-      for(let key in req.body) {
-        params.push(req.body[key]);
-      }
-      var newObj = new modelRoutes.models[model](...params);
+  router.post(`/api/${model}`,jsonParser, function(req, res, next) {
+    debug(`POST: api/${model}`);
 
-      storage.createItem(`${model}`, newObj);
-      header.appHeader(res, 200, newObj);
-
-    } catch (err) {
-      console.error(err);
-      header.textHeader(res, 400, 'Bad request!')
+    let params = [];
+    for(let key in req.body) {
+      params.push(req.body[key]);
     }
+
+    var newObj = new modelRoutes.models[model](...params);
+
+    storage.createItem(`${model}`, newObj)
+    .then(item => res.JSON(item))
+    .catch(err => next(err));
   });
 }
 
 modelRoutes.modelDelete = function(model, router) {
 
-  router.delete(`/api/${model}`, function(req, res) {
+  router.delete(`/api/${model}`,jsonParser, function(req, res, next) {
+    debug(`DELETE: api/${model}`);
     if (req.url.query.id) {
+      
       storage.deleteItem(`${model}`, req.url.query.id)
-      .then( item => {
-        header.appHeader(res, 202, item);
-      })
-      .catch( err => {
-        console.error(err);
-        header.textHeader(res, 404, `${model} not found!`)
-      });
+      .then(item => res.JSON(item))
+      .catch( err => next(err));
 
       return;
     };
-
-    header.textHeader(res, 400, 'Bad request!')
   });
-
 }
-
 
 require('./auto-direct.js')(modelRoutes.models)
--- lab-eddie/server.js
+++ lab-eddie/server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const morgan = require('morgan');
 const createError = require('http-errors');
 const jasonParser = require('body-parser').json();
+const modelPaths = require('./lib/model-paths.js')
 const debug = require('debug')('note:server')
 const PORT = process.env.PORT || 3000;
 
@@ -11,6 +12,10 @@ const app = express();
 
 app.use(morgan('dev'));
 
+const modelKeys = Object.keys(modelPaths.models);
+modelKeys.forEach(key => {
+  modelPaths.allRoutes(key, app);
+})
 
   
 app.listen(PORT, () => {
